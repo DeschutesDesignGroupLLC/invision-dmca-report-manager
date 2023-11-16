a4ad98a05383a1225e97643c52ee6a2a0264d5c2
Jon Erickson <jon@deschutesdesigngroup.com>
2023-11-16 13:18:55 -0800

    Fix copyright infringement ACP members tab error (#3)
    * Fixed infringements chart
    
    * Build 1.0.4

--- src/extensions/core/MemberACPProfileBlocks/CopyrightInfringementChart.php
+++ src/extensions/core/MemberACPProfileBlocks/CopyrightInfringementChart.php
@@ -11,8 +11,13 @@
 
 namespace IPS\dmca\extensions\core\MemberACPProfileBlocks;
 
+use IPS\dmca\Reports\Strike;
 use IPS\dmca\Reports\Report;
+use IPS\Helpers\Chart\Database;
 use IPS\Http\Url;
+use IPS\Member;
+use IPS\Request;
+use IPS\Theme;
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
 if (!\defined('\IPS\SUITE_UNIQUE_KEY')) {
@@ -32,7 +37,7 @@ class _CopyrightInfringementChart extends \IPS\core\MemberACPProfile\Block
      */
     public function output(): string
     {
-        $chart = new \IPS\Helpers\Chart\Database(Url::internal('app=core&module=members&controller=members&do=view')->setQueryString('id', \IPS\Request::i()->id)->setQueryString('tab', 'dmca_CopyrightInfringements'), Report::$databaseTable, 'created_at', '', array(
+        $chart = new Database(Url::internal('app=core&module=members&controller=members&do=view')->setQueryString('id', Request::i()->id)->setQueryString('tab', 'dmca_CopyrightInfringements'), Strike::$databaseTable, 'created_at', '', array(
             'isStacked' => false,
             'backgroundColor' => '#ffffff',
             'hAxis' => array('gridlines' => array('color' => '#f5f5f5')),
@@ -40,13 +45,14 @@ class _CopyrightInfringementChart extends \IPS\core\MemberACPProfile\Block
             'areaOpacity' => 0.4
         ));
 
-        $chart->where[] = ['member_id = ?', \IPS\Request::i()->id];
-        $chart->where[] = ['status = ?', \IPS\dmca\Reports\Report::REPORT_STATUS_APPROVED];
+        $chart->joins[] = ['dmca_reports', 'dmca_reports.id=dmca_strikes.report_id'];
+        $chart->where[] = ['member_id = ?', Request::i()->id];
+        $chart->where[] = ['status = ?', Report::REPORT_STATUS_APPROVED];
 
-        $chart->addSeries(\IPS\Member::loggedIn()->language()->addToStack('dcma_chart_infringements'), 'number', 'COUNT(*)');
-        $chart->title = \IPS\Member::loggedIn()->language()->addToStack('dcma_chart_infringements_title');
+        $chart->addSeries(Member::loggedIn()->language()->addToStack('dcma_chart_infringements'), 'number', 'COUNT(*)');
+        $chart->title = Member::loggedIn()->language()->addToStack('dcma_chart_infringements_title');
         $chart->availableTypes = array( 'AreaChart', 'ColumnChart', 'BarChart' );
 
-        return \IPS\Theme::i()->getTemplate('chart', 'dmca', 'admin')->chart($chart);
+        return Theme::i()->getTemplate('chart', 'dmca', 'admin')->chart($chart);
     }
 }
